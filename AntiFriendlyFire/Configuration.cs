27a846e18def497c33643230064e497fa114d823
TrueThatDev <gamingguy444@gmail.com>
2017-05-27 22:01:06 +0300

    Add project files.

---
+++ AntiFriendlyFire/Configuration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Rocket.API;
+
+namespace AntiFriendlyFire
+{
+    public class Configuration : IRocketPluginConfiguration
+    {
+        public bool Enabled;
+
+        public void LoadDefaults()
+        {
+            Enabled = true;
+        }
+    }
+}
